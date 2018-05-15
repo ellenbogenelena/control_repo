21aed2ca8b174b820475b625eee3a75d1f269f31
ellenbogenelena <39294500+ellenbogenelena@users.noreply.github.com>
2018-05-15 13:07:14 +0000

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -3,6 +3,10 @@ node default {
  
  node ip-172-31-17-234.eu-central-1.compute.internal {
    include role:master_server
+   file { 'root/README':
+      ensure => file,
+      content => $fqdn,
+   }
  }
  
  node ip-172-31-31-200.eu-central-1.compute.internal {
